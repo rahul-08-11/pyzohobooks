f51534fcb6783d9bfaa69c225951ef5f2602ac17
rahul-08-11 <grahulkumar4444@gmail.com>
2024-12-29 17:14:04 +0530

    update v.1.2

--- pyzohobook/invoice.py
+++ pyzohobook/invoice.py
@@ -3,11 +3,22 @@ from .utils import *
 import json
 import requests
 from urllib.parse import urlencode
-
+from typing import Literal
 
 def create_invoice(
     invoice_data: dict, book_token: str, organization_id: str
 ) -> requests.Response:
+    """
+    Creates an invoice in Zoho Books.
+
+    Args:
+        invoice_data (dict): Data for the new invoice.
+        book_token (str): Zoho Books API token.
+        organization_id (str): Organization ID in Zoho Books.
+
+    Returns:
+        requests.Response: Response object containing the ID of the newly created invoice.
+    """
     response = requests.post(
         f"https://www.zohoapis.ca/books/v3/invoices?organization_id={organization_id}",
         headers=get_book_headers(book_token=book_token),
@@ -18,6 +29,19 @@ def create_invoice(
 def update_invoice(
     invoice_id: str, invoice_data: dict, book_token: str, organization_id: str
 ) -> requests.Response:
+
+    """
+    Updates an invoice in Zoho Books.
+
+    Args:
+        invoice_id (str): The ID of the invoice to be updated.
+        invoice_data (dict): The data to update the invoice with.
+        book_token (str): Zoho Books API token.
+        organization_id (str): Organization ID in Zoho Books.
+
+    Returns:
+        requests.Response: Response object containing the result of the update operation.
+    """
 
     response = requests.put(
         f"https://www.zohoapis.ca/books/v3/invoices/{invoice_id}?organization_id={organization_id}",
@@ -31,6 +55,18 @@ def delete_invoice(
     invoice_id: str, book_token: str, organization_id: str
 ) -> requests.Response:
 
+    
+    """
+    Deletes an invoice in Zoho Books.
+
+    Args:
+        invoice_id (str): The ID of the invoice to be deleted.
+        book_token (str): Zoho Books API token.
+        organization_id (str): Organization ID in Zoho Books.
+
+    Returns:
+        requests.Response: Response object containing the result of the delete operation.
+    """
     response = requests.delete(
         f"https://www.zohoapis.ca/books/v3/invoices/{invoice_id}?organization_id={organization_id}",
         headers=get_book_headers(book_token=book_token),
@@ -39,13 +75,93 @@ def delete_invoice(
     return response
 
 def search_invoice(
-    search_params: dict, book_token: str, organization_id: str
+    search_params: dict, book_token: str, organization_id: str,
 ) -> requests.Response:
     
     # Encode search parameters into the query string
+    
+    """
+    Searches for invoices in Zoho Books.
+
+    Args:
+        search_params (dict): Dictionary of search parameters.
+        book_token (str): Zoho Books API token.
+        organization_id (str): Organization ID in Zoho Books.
+
+    Returns:
+        requests.Response: Response object containing the search results.
+
+    Supported search parameters:
+        - invoice_number (str): Search invoices by invoice number.
+        - customer_name (str): Search invoices by customer name.
+        - invoice_status (str): Search invoices by invoice status.
+        - invoice_date (str): Search invoices by invoice date.
+        - due_date (str): Search invoices by due date.
+        - total (str): Search invoices by total amount.
+        - page (int): Page number to retrieve.
+        - per_page (int): Number of records to retrieve per page.
+
+    Note:
+        - The search parameters are case-sensitive.
+    """
     query_string = urlencode(search_params)
     response = requests.get(
         f"https://www.zohoapis.ca/books/v3/invoices?organization_id={organization_id}&{query_string}",
+        headers=get_book_headers(book_token=book_token),
+    )
+
+    return response
+
+
+
+def mark_invoice(
+    book_token: str, invoice_id: str, organization_id: str, status : Literal['sent','draft','void']
+) -> requests.Response:
+
+    
+    """
+    Marks an invoice with a specified status in Zoho Books.
+
+    Args:
+        book_token (str): Zoho Books API token.
+        invoice_id (str): The ID of the invoice to be marked.
+        organization_id (str): Organization ID in Zoho Books.
+        status (Literal['sent','draft','void']): The status to mark the invoice with.
+
+    Returns:
+        requests.Response: Response object containing the result of the mark operation.
+
+    Raises:
+        requests.exceptions.RequestException: If the request to the Zoho Books API fails.
+    """
+
+    response = requests.post(f"https://www.zohoapis.ca/books/v3/invoices/{invoice_id}/status/{status}?organization_id={organization_id}", 
+    headers=get_book_headers(book_token=book_token))
+
+    return response
+
+
+def fetch_invoice(
+    invoice_id: str, book_token: str, organization_id: str, formate : Literal['pdf','json','html']
+) -> requests.Response:
+
+    """
+    Fetches an invoice in Zoho Books.
+
+    Args:
+        invoice_id (str): The ID of the invoice to be fetched.
+        book_token (str): Zoho Books API token.
+        organization_id (str): Organization ID in Zoho Books.
+        formate (Literal['pdf','json','html']): The format of the invoice to be fetched.
+
+    Returns:
+        requests.Response: Response object containing the invoice in the specified format.
+
+    Raises:
+        requests.exceptions.RequestException: If the request to the Zoho Books API fails.
+    """
+    response = requests.get(
+        f"https://www.zohoapis.ca/books/v3/invoices/{formate}?organization_id={organization_id}&invoice_ids={invoice_id}",
         headers=get_book_headers(book_token=book_token),
     )
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyzoho-books",
-    version="0.1.1",
+    version="0.1.2",
     description="A Python package for interacting with the Zoho Books API.",
     author="Rahul Kumar",
     author_email="rahul.work.programming@gmail.com",
